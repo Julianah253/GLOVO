52c8baa167d4aa0898d24c761a08f4e91caa906d
Julianah Babatunde <babatundejulianah03@gmail.com>
2023-04-22 00:13:38 +0100

    GLOVO-ADDED

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/nav/Navbar'
+// import Home from './components/home/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>   
+    </div>
+  )
+}
+
+export default App
