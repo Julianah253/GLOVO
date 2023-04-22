0edb8cdcf9480ccfe14f63fdac6182466657efa2
Julianah Babatunde <babatundejulianah03@gmail.com>
2023-04-22 18:41:17 +0100

    mutiple-breaks-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Nav from './components/nav/Navbar'
+import Rest from './components/restaurants/Rest'
 // import Home from './components/home/Home'
 
 const App = () => {
   return (
     <div>
       <Nav/>   
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <Rest/>
     </div>
   )
 }
---
+++ src/components/restaurants/Rest.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import './rest.css'
+const Rest = () => {
+  return (
+    <div>
+        <div class="restaurants">
+            <h2> Top restaurants and more in Nigeria </h2>
+
+            <br/><br/>
+            
+            <div className="top">
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/hlwqevdvj0cfzss5gpoi"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/egmjz07ibkbr9uopccsc"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/jui8atcdisz1fyj8xqif"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/e0pcegpluhtq0akciakd"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bottom">
+            <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/o57akgxyabkgo04kjlbu"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/zdpfnwewlbzc40surjgw"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/nem66tghxmqigr2fodaq"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+
+                <div class="image-text">
+                        <img src="https://res.cloudinary.com/glovoapp/q_30,f_auto,c_fill,dpr_1.0,h_128,w_128,b_transparent/Stores/cg4sdskoaecoxhp7lhkt"/>
+                    <div className="text">
+                        <h3> The Place </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Rest
